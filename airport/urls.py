ae7fce6d5b1bc2958dba836d780d0b08cc28aad4
Lubart2517 <lubomirvihvatniuk@gmail.com>
2024-01-17 20:10:57 +0200

    add flight and route ViewSets

--- airport/urls.py
+++ airport/urls.py
@@ -4,13 +4,18 @@ from rest_framework import routers
 from airport.views import (
     AirplaneViewSet,
     AirportViewSet,
+    AirplaneTypeViewSet,
+    RouteViewSet,
+    FlightViewSet
 )
 
 
 router = routers.DefaultRouter()
 router.register("airports", AirportViewSet)
 router.register("airplanes", AirplaneViewSet)
-
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("routes", RouteViewSet)
+router.register("flights", FlightViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
@@ -44,7 +46,7 @@ class AirportViewSet(
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
     def get_queryset(self):
-        """Retrieve the profiles with filters"""
+        """Retrieve the airports with filters"""
         name = self.request.query_params.get("name")
         city = self.request.query_params.get("city")
 
@@ -88,7 +90,7 @@ class AirplaneViewSet(
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
     def get_queryset(self):
-        """Retrieve the profiles with filters"""
+        """Retrieve the airplanes with filters"""
         name = self.request.query_params.get("name")
         airplane_type = self.request.query_params.get("airplane_type")
 
@@ -113,6 +115,131 @@ class AirplaneViewSet(
                 "airplane_type",
                 type=OpenApiTypes.STR,
                 description="Filter by airplane type (ex. ?airplane_type=Airbus)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class AirplaneTypeViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = AirplaneType.objects
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class RouteViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Route.objects
+    serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_queryset(self):
+        """Retrieve the routes with filters"""
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
+        queryset = self.queryset
+
+        if source:
+            queryset = queryset.filter(source__icontains=source)
+
+        if destination:
+            queryset = queryset.filter(destination__icontains=destination)
+
+        return queryset.distinct()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.STR,
+                description="Filter by route source (ex. ?source=Orly)",
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.STR,
+                description="Filter by route destination (ex. ?destination=Orly)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class FlightViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Flight.objects
+    serializer_class = FlightSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_queryset(self):
+        """Retrieve the flights with filters"""
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        date_format = "%Y-%m-%d"
+
+        departure_day = self.request.query_params.get("departure_day")
+        arrival_day = self.request.query_params.get("arrival_day")
+
+        queryset = self.queryset
+
+        if source:
+            queryset = queryset.filter(route__source__name__icontains=source)
+
+        if destination:
+            queryset = queryset.filter(route__destination__name__icontains=destination)
+
+        if departure_day:
+            day = datetime.datetime.strptime(departure_day, date_format)
+            queryset = queryset.filter(departure_time__date=day)
+
+        if arrival_day:
+            day = datetime.datetime.strptime(arrival_day, date_format)
+            queryset = queryset.filter(arrival_time__date=day)
+
+        return queryset.distinct()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.STR,
+                description="Filter by flight route source (ex. ?source=Orly)",
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.STR,
+                description="Filter by flight route destination (ex. ?destination=Orly)",
+            ),
+            OpenApiParameter(
+                "departure_day",
+                type=OpenApiTypes.STR,
+                description="Filter by flight departure day (ex. ?departure_time.day=2024-01-22)",
+            ),
+            OpenApiParameter(
+                "arrival_day",
+                type=OpenApiTypes.STR,
+                description="Filter by flight arrival day (ex. ?arrival_time.day=2024-01-22)",
             ),
         ]
     )
